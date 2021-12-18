30846e7180fe926c574aab91e52f597e5b3fae65
shinkats <qwerty.mnbvcx2009@gmail.com>
2021-12-19 00:46:32 +0900

    不要だがsessionredisプロジェクトと合わせるためにSerializable interfaceを付与

--- src/main/kotlin/com/example/nosessionjwt/security/LoginUser.kt
+++ src/main/kotlin/com/example/nosessionjwt/security/LoginUser.kt
@@ -1,7 +1,8 @@
 package com.example.nosessionjwt.security
 
 import org.springframework.security.core.authority.SimpleGrantedAuthority
+import java.io.Serializable
 
-class LoginUser(val id: Int, roles: List<String>) {
+class LoginUser(val id: Int, roles: List<String>) : Serializable {
     val authorities = roles.map { SimpleGrantedAuthority(it) }
 }
