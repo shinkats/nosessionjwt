6e4f42b732964131a18e39ec70f30c437012fde3
shinkats <qwerty.mnbvcx2009@gmail.com>
2021-12-25 23:37:23 +0900

    autoApply should be false

--- src/main/kotlin/com/example/nosessionjwt/jpa/converter/JpaConverterJson.kt
+++ src/main/kotlin/com/example/nosessionjwt/jpa/converter/JpaConverterJson.kt
@@ -4,13 +4,11 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import javax.persistence.AttributeConverter
 import javax.persistence.Converter
 
-@Converter(autoApply = true)
+@Converter
 class JpaConverterJson : AttributeConverter<Any?, String?> {
     private val objectMapper = ObjectMapper()
 
-    override fun convertToDatabaseColumn(meta: Any?): String? =
-        objectMapper.writeValueAsString(meta)
+    override fun convertToDatabaseColumn(meta: Any?): String? = objectMapper.writeValueAsString(meta)
 
-    override fun convertToEntityAttribute(dbData: String?): Any? =
-        objectMapper.readValue(dbData, Any::class.java)
+    override fun convertToEntityAttribute(dbData: String?): Any? = objectMapper.readValue(dbData, Any::class.java)
 }
